4e41910bb94f6db3e981a33e8bca789e5a563b68
steel25baby <bridgetgitonga73@gmail.com>
2024-06-03 10:39:08 +0300

    about us section

--- src/Pages/Home/About.jsx
+++ src/Pages/Home/About.jsx
@@ -1,9 +1,39 @@
 import React from 'react'
+import aboutimage from "../../assets/Animation.jpg"
+import { MdComputer } from "react-icons/md";
+
+const Aboutus = ({ icon}) => {
+    return (
+         
+      <div className="Aboutus">
+        <div className="Aboutus-icon-wrapper">{icon}</div>
+      </div>
+    );
+  };
 
 const About = () => {
   return (
     <section className='about-section'>
+        <div className='imageAbout'><img src={aboutimage} alt="" /></div>
+        <div className='aboutDetails'>
+            <h1>About us</h1>
+            <div>
+                <MdComputer/>
+                <h3 className='detailHeader'>Free training</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, labore maiores! Provident accusantium totam nulla laudantium nisi laboriosam quisquam fuga autem neque incidunt, cum quaerat libero quibusdam beatae magni tempore!</p>
+            </div>
+            <div>
+                <h3 className='detailHeader'>Free training</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, labore maiores! Provident accusantium totam nulla laudantium nisi laboriosam quisquam fuga autem neque incidunt, cum quaerat libero quibusdam beatae magni tempore!</p>
+            </div>
 
+            <div>
+                <h3 className='detailHeader'>Free training</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, labore maiores! Provident accusantium totam nulla laudantium nisi laboriosam quisquam fuga autem neque incidunt, cum quaerat libero quibusdam beatae magni tempore!</p>
+            </div>
+
+        </div>
+        <div></div>
     </section>
   )
 }
--- src/Pages/Home/WhyUs.jsx
+++ src/Pages/Home/WhyUs.jsx
@@ -1,6 +1,6 @@
 import { PiStudentBold } from "react-icons/pi";
 import React from 'react'
-const Why = ({ icon, title, description, }) => {
+const Why = ({ icon, title, description }) => {
     return (
          
       <div className="Why">
@@ -13,9 +13,9 @@ const Why = ({ icon, title, description, }) => {
 const WhyUs = () => {
   return (
     <section className='why-us-section'>
-        <Why  icon={<PiStudentBold />} title="mission" description="to promote a tech based culture through out campus"/>
-        <Why icon={<PiStudentBold />} title="mission" description="to promote a tech based culture through out campus"/>
-        <Why  icon={<PiStudentBold />} title="mission" description="to promote a tech based culture through out campus"/>
+        <Why  icon={<PiStudentBold />} title="mission" description="to promote a tech based culture through out campus. to all tech students and others intersted in tech"/>
+        <Why icon={<PiStudentBold />} title="objectives" description="to make it a simpler for you,to make it seem more real to make your dreams come true "/>
+        <Why  icon={<PiStudentBold />} title="values" description="the realm of technology discipline consisitency learning and colaboration"/>
 <why/>
     </section>
   )
